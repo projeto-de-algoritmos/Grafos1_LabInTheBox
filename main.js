88d3684dea806be560d8366659586d91e7c506d2
Bruno Oliveira <brunooliveira.unb@gmail.com>
2023-09-19 10:41:30 -0300

    criação das células do tabuleiro

--- main.js
+++ main.js
@@ -19,3 +19,34 @@ function paintLine(x1, y1, x2, y2) {
 let c = 50; // tamanho de cada célula
 let rows = canvas.width / c;
 let columns = canvas.height / c;
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.ang = [
+      { x: x * c, y: y * c },
+      { x: x * c + c, y: y * c },
+      { x: x * c + c, y: y * c + c },
+      { x: x * c, y: y * c + c },
+      { x: x * c, y: y * c },
+    ];
+  }
+}
+
+let cells = []; // array de células vazia
+
+for (let i = 0; i < columns; i++) {
+  for (let j = 0; j < rows; j++) {
+    let cell = new Cell(i, j); // passando os valores para criação da célula
+    cells.push(cell); // passando o valor da célula para a array
+  }
+}
+
+// função que faz a criação do campo onde será feito o labirinto
+function lab() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].render();
+  }
+}
